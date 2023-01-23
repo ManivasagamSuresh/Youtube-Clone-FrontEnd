8dba2adf128045539d37b654e2531b789e6fcd7a
ManivasagamSuresh <s.kishore123.64@gmail.com>
2023-01-23 22:13:49 +0530

    ui done

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { useFormik } from 'formik';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import "./Login.css"
+import { Config } from '../../Config'
+
+function Login() {
+    const navigate = useNavigate();
+
+const [User,setUser]=useState({});
+
+const formik = useFormik({
+  initialValues :{
+    email : "",
+    password : "",
+  },
+  validate:(values)=>{
+    let error = {};
+    if(!values.email){
+      error.email = "Please enter Your Email"
+    }
+    if((values.email)&&(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email))){
+      error.email =  "Please enter valid Email"
+    }
+    if(!values.password){
+      error.password = "Please enter your Password"
+    }
+    return error;
+  },
+  onSubmit : async(values)=>{
+    console.log(values);
+      let user = await axios.post(`${Config.api}/signin`,values)
+      setUser(user.data)
+      console.log(User);
+  }
+})
+
+
+
+  return (
+    <div className='Login-Container'>
+      <form onSubmit={formik.handleSubmit}>
+        <div className='Login-Wrapper'>
+        <h1 className='Login-Title'>Login with Email</h1>
+        <div className='Login-SubTitle'>to continue to MBTube</div>
+        <input className='Login-Input' placeholder="E-mail" name='email'  
+                                                            onChange={formik.handleChange} 
+                                                            onBlur={formik.handleBlur} 
+                                                            value={formik.values.email} />
+               {formik.touched.email && formik.errors.email 
+               ? <span style={{color:"#aaaaaa",fontSize:"12px",marginBottom:"15px"}}>{formik.errors.email}</span>
+               : null
+               }                                             
+        <input className='Login-Input' type={"password"} placeholder="Password"
+                                                             name='password'  
+                                                             onChange={formik.handleChange} 
+                                                             onBlur={formik.handleBlur} 
+                                                             value={formik.values.password}/>
+                {formik.touched.password && formik.errors.password 
+               ? <span style={{color:"#aaaaaa",fontSize:"12px",marginBottom:"15px"}}>{formik.errors.password}</span>
+               : null
+               }
+        <button type={"submit"} className='Login-Button'>Login</button>
+        <div>Don't have an account ?</div><span className='Login-Signup' onClick={()=>{navigate('/Signup')}}>Sign up..</span>
+        </div>
+        </form>
+        </div>
+  )
+}
+
+export default Login
---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,83 @@
+import axios from 'axios';
+import { useFormik } from 'formik';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Config } from '../../Config';
+import "./Signup.css"
+
+
+function Signup() {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+      initialValues :{
+        name:"",
+        email : "",
+        password : "",
+      },
+      validate:(values)=>{
+        let error = {};
+        if(!values.name){
+          error.name = "Please enter your name"
+        }
+        if(!values.email){
+          error.email = "Please enter Your Email"
+        }
+        if((values.email)&&(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email))){
+          error.email =  "Please enter valid Email"
+        }
+        if(!values.password){
+          error.password = "Please enter your Password"
+        }
+        return error;
+      },
+      onSubmit : async(values)=>{
+        console.log(values);
+          let user = await axios.post(`${Config.api}/signup`,values)
+          // setUser(user.data)
+          // console.log(user);
+      }
+    })
+    
+    
+
+
+  return (
+    <div className='Signup-Container'>
+      <form onSubmit={formik.handleSubmit}>
+        <div className='Signup-Wrapper'>
+        <h1 className='Signup-Title'>Welcome..!</h1>
+        <div className='Signup-SubTitle'>Signup with Email</div>
+        <input className='Signup-Input' placeholder="User Name"name='name'  
+                                                             onChange={formik.handleChange} 
+                                                             onBlur={formik.handleBlur} 
+                                                             value={formik.values.name}/>
+                {formik.touched.name && formik.errors.name 
+               ? <span style={{color:"#aaaaaa",fontSize:"12px",marginBottom:"15px"}}>{formik.errors.name}</span>
+               : null
+               }
+        <input className='Signup-Input' placeholder="E-mail"  name='email'  
+                                                            onChange={formik.handleChange} 
+                                                            onBlur={formik.handleBlur} 
+                                                            value={formik.values.email}/>
+               {formik.touched.email && formik.errors.email 
+               ? <span style={{color:"#aaaaaa",fontSize:"12px",marginBottom:"15px"}}>{formik.errors.email}</span>
+               : null
+               }                                               
+        <input className='Signup-Input' type={"password"} placeholder="Password"name='password'  
+                                                             onChange={formik.handleChange} 
+                                                             onBlur={formik.handleBlur} 
+                                                             value={formik.values.password}/>
+                {formik.touched.password && formik.errors.password 
+               ? <span style={{color:"#aaaaaa",fontSize:"12px",marginBottom:"15px"}}>{formik.errors.password}</span>
+               : null
+               }
+        <button type='submit' className='Signup-Button'>Register</button>
+        <div>Already have an account ?</div><span className='Signup-Login' onClick={()=>{navigate('/Login')}}>Login..</span>
+        </div>
+        </form>
+        </div>
+  )
+}
+
+export default Signup
